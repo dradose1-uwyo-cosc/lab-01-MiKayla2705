140a13856ffe0f102979b47a085749ebf7a210f1
MiKayla2705 <mrober61@uwyo.edu>
2024-09-17 22:06:04 +0000

    Lab02

---
+++ cosc1010-lab02.py
@@ -0,0 +1,57 @@
+# MiKayla Robertson
+# UWYO COSC 1010
+# Submission Date 09/17/2024
+# Lab 02 
+# Lab Section: 10
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+your_variable_here = "when you see this, replace it with your code"
+
+## Section ONE
+
+# Complete the following print statement to print out "Hello, COSC1010"
+print("Hello, COSC1010")
+
+# Assign the string above to a variable named hello_message and print that variable
+hello_message = "Hello, COSC1010"
+print("Hello, COSC1010")
+
+# Assign the string "cowboy joe" to a variable, and print that variable with title casing
+uw_spirit = "cowboy joe"
+print(uw_spirit)
+
+# Complete the following f-string print message 
+    # You will need to create your own variables and insert them  
+    # the final message should read `The University of Wyoming was founded in 1886`
+school = "University of Wyoming"
+time = "1886'"
+print(f"The {school} was founded in {time}")
+
+# Now let's do some math with variables 
+    # Create two variables x and y and assign them the values 5 and 10 respectively 
+    # Complete the following print statements using your variables
+    #All math must be done within the braces in the f-strings
+number01 = "15"
+print(f"x + y = {number01}")
+number02 = "-5"
+print(f"x - y = {number02}")
+number03 = "50"
+print(f"x * y = {number03}")
+number04 = "0.5"
+print(f"x / y = {number04}")
+
+# String concatenation 
+    # Finally we will take a look at string concatenation
+    # String concatenation combines two strings together
+    # It is done using the + operator
+    # Create three variables:
+        # first_name, which is your first name 
+        # last_name, which is your last name
+        # space, which is a space character 
+    # Use string concatenation to print out your full name 
+first_name = "MiKayla"
+last_name = "Robertson"
+print("my name is:", first_name, " ",last_name)
